e493b438b3030c562cb7043025781e83bf2e483b
Jonan Oribe <jonan.oribe@gmail.com>
2019-10-21 19:02:35 +0200

    Basics

--- src/app/components/products/product-list/product-list.component.ts
+++ src/app/components/products/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService} from '../../../services/product.service';
+import { Product } from 'src/app/models/product';
 
 @Component({
   selector: 'app-product-list',
@@ -7,8 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  productList: Product[];
 
-  ngOnInit() {}
+  constructor(private productService:ProductService) { }
+
+  ngOnInit() {
+
+    this.productService.getProducts()
+    .snapshotChanges()
+    .subscribe(item=>{
+      this.productList=[];
+      item.forEach(element=>{
+        let x=element.payload.toJSON();
+        x["$key"]= element.key;
+        this.productList.push(x as Product);
+      })
+    });
+
+  }
+
+  onEdit(product: Product){
+
+  }
+
+  onDelete($key: string){
+    
+  }
 
 }
