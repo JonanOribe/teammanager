7cc8243058695b7b85fa6650a0c39ee9830ea9c8
Jonan Oribe <jonan.oribe@gmail.com>
2019-10-22 15:11:12 +0200

    Next step: task-list

--- src/app/components/tasks/task/task.component.ts
+++ src/app/components/tasks/task/task.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../../services/task.service';
+import { NgForm } from '@angular/forms';
+import { Task } from 'src/app/models/task';
+import { of } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-task',
@@ -7,8 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskComponent implements OnInit {
 
-  constructor() { }
+  constructor(private taskService: TaskService,
+    private toastr: ToastrService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.taskService.getTasks();
+    this.resetFrom();
+  }
+
+  onSubmit(taskForm: NgForm)
+  {
+    if(taskForm.value.$key==null){
+      this.taskService.insertTask(taskForm.value);
+      this.resetFrom(taskForm);
+    }else{
+      this.taskService.updateTask(taskForm.value);
+      this.resetFrom(taskForm);
+    }
+    this.toastr.success('Successful Operation','Successful Operation')
+
+  }
+
+  resetFrom(taskForm?: NgForm)
+  {
+    if(taskForm != null){
+      taskForm.reset();
+      this.taskService.selectedTask= new Task();
+    }
+  }
 
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase,AngularFireList} from 'angularfire2/database';
+import { Task }  from '../models/task';
+import { TaskComponent } from '../components/tasks/task/task.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  taskList: AngularFireList<any>;
+  selectedTask:  Task= new Task();
+
+  constructor(private firebase: AngularFireDatabase) {}
+
+  getTasks(){
+    this.taskList=this.firebase.list('tasks');
+    return this.taskList;
+  }
+
+  insertTask(task: Task){
+    this.taskList.push({
+      name: task.name,
+      location: task.location,
+      hours: task.hours
+    });
+  }
+
+  updateTask(task:Task){
+    this.taskList.update(task.$key,{
+      name: task.name,
+      location: task.location,
+      hours: task.hours
+    })
+  }
+
+  deleteTask($key:string)
+  {
+    this.taskList.remove($key);
+  }
 }
